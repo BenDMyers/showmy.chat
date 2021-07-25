0550ec5bd8948d00bafd77a53568b6c12e96f404
Ben Myers <ben.myers@okstate.edu>
2021-07-25 09:53:35 -0500

    Okay, you don't need a plugin for this

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,6 @@
 const {EleventyServerlessBundlerPlugin} = require('@11ty/eleventy');
 
 // const twitchChat = require('eleventy-plugin-twitch-chat');
-const injectComfy = require(`${process.cwd()}/src/plugin/src/inject-comfy`);
-const addTwitchChat = require(`${process.cwd()}/src/plugin/src/twitch-chat-shortcode`);
 
 module.exports = function(eleventyConfig) {
 	eleventyConfig.addPlugin(EleventyServerlessBundlerPlugin, {
@@ -12,10 +10,8 @@ module.exports = function(eleventyConfig) {
 	});
 
 	// eleventyConfig.addPlugin(twitchChat);
-	eleventyConfig.addTransform('inject-comfy', injectComfy);
-	eleventyConfig.addShortcode('twitchChat', addTwitchChat);
 
-	eleventyConfig.addPassthroughCopy('src/plugin');
+	eleventyConfig.addPassthroughCopy('src/scripts');
 	eleventyConfig.addPassthroughCopy('src/themes');
 
 	return {
--- src/plugin/README.md
+++
@@ -1,8 +0,0 @@
----
-permalink: false
----
-# `eleventy-plugin-twitch-chat` Temporary Port
-
-Traditionally, [Eleventy transforms](https://www.11ty.dev/docs/config/#transforms) use file output paths to determine whether the file type is appropriate for transformations. However, serverless templates don't currently support `outputPath` (see 11ty/eleventy#1901).
-
-In the meantime, this is a temporary local port of [`eleventy-plugin-twitch-chat`](https://npmjs.com/package/eleventy-plugin-twitch-chat), whose transform has been modified to assume that *all* transformed files are HTML. This definitely isn't a safe assumption for a production-ready plugin, but I think it'll apply for long enough here.
