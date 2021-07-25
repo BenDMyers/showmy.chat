60c7c289ff56352a2b59158875851dd2c220514b
Ben Myers <ben.myers@okstate.edu>
2021-07-25 09:10:01 -0500

    Toggle themes with query parameters

--- src/chat/chatbox.11tydata.js
+++ src/chat/chatbox.11tydata.js
@@ -1,3 +1,41 @@
+const validUrl = require('valid-url');
+
+/**
+ * Takes chosen theme and determines stylesheet href
+ * @param {{
+ * 		eleventy?: {
+ * 			serverless?: {
+ * 				path: {},
+ * 				query: {
+ * 					theme?: string
+ * 				}
+ * 			}
+ * 		},
+ * 		themes: {
+ * 			list: string[],
+ * 			normalized: Object<string, true>
+ * 		}
+ * }} data
+ * @returns {string} valid href for chosen theme
+ */
+function parseTheme(data) {
+	let themes = data.themes;
+	let theme = data.eleventy?.serverless?.query?.theme || 'default';
+
+	if (validUrl.isWebUri(theme)) {
+		return theme;
+	} else if (themes.normalized[theme]) {
+		return `/themes/${theme}.css`;
+	} else {
+		return `/themes/default.css`;
+	}
+}
+
 module.exports = {
-	theme: 'default'
+	eleventyComputed: {
+		themeStylesheet: data => parseTheme(data)
+	},
+	permalink: {
+		serverless: '/c/:channel.html'
+	}
 };
