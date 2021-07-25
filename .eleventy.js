59ec0572928296f041757fbf3a6f4a1f0d5848b2
Ben Myers <ben.myers@okstate.edu>
2021-07-25 10:37:16 -0500

    Generate stylesheet href with filter, not computed data

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,5 @@
 const {EleventyServerlessBundlerPlugin} = require('@11ty/eleventy');
+const {isWebUri} = require('valid-url');
 
 // const twitchChat = require('eleventy-plugin-twitch-chat');
 
@@ -15,7 +16,9 @@ module.exports = function(eleventyConfig) {
 	eleventyConfig.addPassthroughCopy('src/scripts');
 	eleventyConfig.addPassthroughCopy('src/themes');
 
-	eleventyConfig.addFilter('out', output => JSON.stringify(output, null, 2));
+	eleventyConfig.addFilter('debug', output => JSON.stringify(output, null, 2));
+
+	eleventyConfig.addFilter('toStylesheet', toStylesheet)
 
 	return {
 		dir: {
@@ -23,3 +26,34 @@ module.exports = function(eleventyConfig) {
 		}
 	};
 };
+
+/**
+ * Takes chosen theme and determines stylesheet href
+ * @param {{
+ * 		eleventy?: {
+ * 			serverless?: {
+ * 				path: {},
+ * 				query: {
+ * 					theme?: string
+ * 				}
+ * 			}
+ * 		},
+ * 		themes: {
+ * 			list: string[],
+ * 			normalized: Object<string, true>
+ * 		}
+ * }} data
+ * @returns {string} valid href for chosen theme
+ */
+ function toStylesheet(theme, themes) {
+	// let themes = data.themes;
+	// let theme = data.eleventy?.serverless?.query?.theme || 'default';
+
+	if (isWebUri(theme)) {
+		return theme;
+	} else if (themes.normalized[theme]) {
+		return `/themes/${theme}.css`;
+	} else {
+		return `/themes/default.css`;
+	}
+}
