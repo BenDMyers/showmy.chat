3bcca5646f40d53652d39a65c284ee539db0b59b
Ken aka Frosty <90424167+KenAKAFrosty@users.noreply.github.com>
2022-01-11 23:45:24 -0500

    BTTV Functionality (and jest/tests setup for use elsewhere now too) (#46)
    * rfx: slight clarity on 'output' variable name
    
    * fix: workaround for chromium 75 regex bug
    
    * add: more punctutation to regex negation set
    
    * add: jest for testing, start of BTTV integration
    
    * add: bttv integration functionality and tests
    
    * add: bttv functionality live on chatbox
    
    * add: gets global bttv emotes now as well
    
    * fix: running .json() on fetch responses awaited
    
    * rfx: more appropriate name for global dict
    
    * rfx: more appropriate name for global bttv emote dict
    
    * rfx: moved sample constants to within describe func
    
    * rfs: destructured inputs in bttvIntegration.test.js
    
    * rfx: samples used in more than 1 describes. moved global but away from top of file still
    
    * fix: removed test variable
    
    * fix: went with returning null to be consistent. tests updated
    
    * add: jsdocs for replacekeywords, better naming for input validation
    
    * fix: truthy strings check accepts array now
    
    * fix: areTruthyStrings now uses array passed as argument, not arguments variable
    
    * add: bttv specificity to replacekeyword - including plus bit of API
    
    * add: JSDocs notation for bttvIntegration.js
    
    * fix: typo + clarity
    
    * rfx: replacekeyword includes html, just calling it a string might be misleading
    
    * rfx: cleanedup JSDocs, skipped unnecessary emotesArray
    
    * Update curly braces in JSDocs
    
    Co-authored-by: Ben Myers <ben@benmyers.dev>

--- src/scripts/handleChat.js
+++ src/scripts/handleChat.js
@@ -3,6 +3,7 @@ const watchedChannels = chatbox.getAttribute('data-twitch-chat');
 
 let mostRecentSender = '';
 let currentMessageGroup = 0;
+let bttvEmoteDict = {}
 
 function htmlEntities(html) {
 	function it() {
@@ -51,6 +52,22 @@ function formatEmotes(text, emotes = {}) {
 	return htmlEntities(splitText).join('')
 }
 
+function formatBttvEmotes(text){ 
+	for (key in bttvEmoteDict){ 
+		if (text.includes(key)){ 
+			const bttvId = bttvEmoteDict[key]
+			text = replaceKeywordWithEmoteImageString(
+				text,
+				key,
+				getBttvImageUrl(bttvId),
+				bttvId
+			)
+		}
+	}
+	return text
+}
+
+
 /**
  * @param {string} messageContents
  * @returns {string} message with any user mentioned wrapped in <mark> tags
@@ -87,6 +104,7 @@ ComfyJS.onChat = function(user, messageContents, flags, self, extra) {
 
 	const message = document.createElement('div');
 	let formattedMessage = formatEmotes(messageContents, extra.messageEmotes);
+	formattedMessage = formatBttvEmotes(formattedMessage);
 	// formattedMessage = formatLinks(formattedMessage);
 	formattedMessage = formatUserMentions(formattedMessage);
 
@@ -160,6 +178,7 @@ ComfyJS.onMessageDeleted = function(id, extra) {
 	if (messageToDelete) {
 		removeMessageFromDomAndShiftOthers(messageToDelete);
 	}
+	messageToDelete.remove();
 }
 
 function removeMessageFromDomAndShiftOthers(messageToDelete) {
@@ -172,4 +191,12 @@ function removeMessageFromDomAndShiftOthers(messageToDelete) {
 	messageToDelete.remove();
 }
 
-ComfyJS.Init(null, null, watchedChannels.split(' '));
+
+async function init(){
+	const twitchUserId = await getTwitchUserId(watchedChannels.split(' ')[0]);
+	bttvEmoteDict = await getBttvChannelEmoteDict(twitchUserId);
+	bttvEmoteDict = await addGlobalBttvEmotesToDict(bttvEmoteDict);
+	ComfyJS.Init(null, null, watchedChannels.split(' '));
+}
+
+init();
