2dafd1dd5007eda260be8f732e7cd41cc77c46ab
Nicky Meuleman <nicky.dev@outlook.com>
2022-01-16 15:33:19 +0100

    set up linting and formatting

---
+++ .stylelintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+	"extends": ["stylelint-config-recommended", "stylelint-config-prettier"],
+	"overrides": [
+		{
+			"files": ["**/*.html"],
+			"customSyntax": "postcss-html"
+		}
+	]
+}
