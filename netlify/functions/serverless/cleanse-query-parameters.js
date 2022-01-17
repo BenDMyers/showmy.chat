92de38ed840ccab40bf3f936bf5ba6544c84bda7
Nicky Meuleman <nicky.dev@outlook.com>
2022-01-17 18:04:05 +0100

    lint and format entire project

--- netlify/functions/serverless/cleanse-query-parameters.js
+++ netlify/functions/serverless/cleanse-query-parameters.js
@@ -1,40 +1,41 @@
 /**
- * @type {Object<string, {validate: Validator, transform?: Transformer}}
+ * @type {object<string, {validate: Validator, transform?: Transformer}>}
  */
 const VALID_PARAMETERS = {
 	DEMO: {
 		validate: isBoolean,
-		transform: toBoolean
+		transform: toBoolean,
 	},
 	showCommands: {
-		validate: value => (isBoolean(value) || isCommaSeparatedList(value)),
-		transform: value => {
+		validate: (value) => isBoolean(value) || isCommaSeparatedList(value),
+		transform: (value) => {
 			if (isBoolean(value)) {
 				return toBoolean(value);
 			}
 			return value.split(',');
-		}
+		},
 	},
 	showLatestMessages: {
 		validate: isPositiveInteger,
-		transform: Number.parseInt
+		transform: Number.parseInt,
 	},
 	theme: {
-		validate: isString
-	}
+		validate: isString,
+	},
 };
 
 /**
  * Iterate over all provided query parameters and determine whether they're valid values,
  * and returns an object where invalid parameters are removed and other parameters are reformatted.
- * @param {Object<string, string>} queryStringParameters
- * @return {Object<string, any>} reformatted query parameters
+ *
+ * @param {object<string, string>} queryStringParameters
+ * @returns {object<string, any>} reformatted query parameters
  */
 function cleanseQueryParameters(queryStringParameters) {
 	// Preload configurations with OPTIONAL defaults
 	const queryParameters = {
 		showLatestMessages: 100,
-		theme: 'default'
+		theme: 'default',
 	};
 
 	// Iterate over provided query parameters
@@ -49,10 +50,14 @@ function cleanseQueryParameters(queryStringParameters) {
 				const newValue = transform ? transform(paramValue) : paramValue;
 				queryParameters[param] = newValue;
 			} else {
-				console.error(`"${paramValue}" is not a valid value for the "${param}" configuration.`);
+				console.error(
+					`"${paramValue}" is not a valid value for the "${param}" configuration.`
+				);
 			}
 		} else {
-			console.error(`"${param}" is not a valid configuration for a showmy.chat overlay`);
+			console.error(
+				`"${param}" is not a valid configuration for a showmy.chat overlay`
+			);
 		}
 	}
 	return queryParameters;
@@ -86,8 +91,6 @@ function isPositiveInteger(value) {
 	const num = Number(value);
 	return Number.isInteger(num) && num > 0;
 }
-
-
 
 // TRANSFORMERS FOR QUERY PARAMETER VALUES
 
--- src/_data/themes.js
+++ src/_data/themes.js
@@ -5,16 +5,16 @@ const themesDirectory = path.join(__dirname, '..', 'themes');
 
 const list = fs
 	.readdirSync(themesDirectory)
-	.filter(stylesheet => stylesheet.endsWith('.css'))
-	.map(stylesheet => stylesheet.replace('.css', ''))
+	.filter((stylesheet) => stylesheet.endsWith('.css'))
+	.map((stylesheet) => stylesheet.replace('.css', ''))
 	.sort();
 
-const normalized = list.reduce(function(accumulated, current) {
+const normalized = list.reduce(function (accumulated, current) {
 	accumulated[current] = true;
 	return accumulated;
 }, {});
 
 module.exports = {
 	list,
-	normalized
+	normalized,
 };
