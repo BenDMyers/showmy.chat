c75c0ecd1e32cd1ca5a343353368054e799dd5f7
Nicky Meuleman <nicky.dev@outlook.com>
2022-01-17 17:35:39 +0100

    undo test push settings

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,11 +1,9 @@
 module.exports = {
 	extends: ['stylelint-config-recommended', 'stylelint-config-prettier'],
-	rules: {'alpha-value-notation': 'percentage'},
 	overrides: [
 		{
 			files: ['**/*.html'],
 			customSyntax: 'postcss-html',
-			rules: {'alpha-value-notation': 'percentage'},
 		},
 	],
 };
