fb644d2e0eb7af168d774d9d07c7c328b72a7e49
Ben Myers <ben.myers@okstate.edu>
2022-05-24 23:36:20 -0500

    Add a comment

--- src/scripts/handleChat.mjs
+++ src/scripts/handleChat.mjs
@@ -88,6 +88,7 @@ function formatEmotes(text, emotes = {}) {
 			splitText.splice(
 				start,
 				emoteName.length,
+				// Insert tags and enough empty strings to ensure splitText's length stays the same
 				markup,
 				...emptyPaddingElements
 			);
