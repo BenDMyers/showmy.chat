8012216d8fbcda91a04ff5b17b57462db205574f
Ben Myers <ben.myers@okstate.edu>
2022-06-03 10:13:18 -0500

    Meta tags for overlay pages themselves

--- src/chat/chatbox.11tydata.js
+++ src/chat/chatbox.11tydata.js
@@ -1,5 +1,35 @@
+const {titleCase} = require('title-case');
+
+/**
+ * Formats a theme's name as title case.
+ *
+ * @param {string} theme lowercase, dash-delimited theme name
+ * @returns {string} theme name, formatted in title case
+ */
+function formatThemeName(theme) {
+	return titleCase(theme.replace(/-/g, ' '));
+}
+
 module.exports = {
 	permalink: {
 		serverless: '/c/:channel',
+	},
+	eleventyComputed: {
+		description: (data) => {
+			if (!data.eleventy.serverless) {
+				return 'Check out this custom chat overlay built with showmy.chat!';
+			}
+
+			const {channel} = data.eleventy.serverless.path;
+			const theme =
+				data.eleventy.serverless.query && data.eleventy.serverless.query.theme;
+
+			if (theme && data.themes.normalized[theme] && theme !== 'default') {
+				const formattedTheme = formatThemeName(theme);
+				return `Check out ${channel}'s custom chat overlay built with showmy.chat's ${formattedTheme} theme!`;
+			}
+
+			return `Check out ${channel}'s custom chat overlay built with showmy.chat!`;
+		},
 	},
 };
